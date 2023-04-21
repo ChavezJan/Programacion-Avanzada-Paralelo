1d937f636ea16f6d43e823dc1574102d8d73b096
Jorge Chavez <chavejoa@gmail.com>
2023-04-20 23:03:15 -0600

    name correction

--- Parcial 2 - Practica 3/practica3.cu
+++ Parcial 2 - Practica 3/practica3.cu
@@ -1,163 +1,92 @@
+%%cu
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
-#include <random>
 #include <stdio.h>
 #include <iostream>
 #include <time.h>
 
-using namespace std;
+#define BLOCK_SIZE 1024
 
-// Errores
+__global__ void search(int* a, int n, int* pos, int find) {
+    __shared__ int shared_a[BLOCK_SIZE];
 
-#define GPUErrorAssertion(ans){gpuAssert((ans),__FILE__, __LINE__);}
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    int start = blockIdx.x * blockDim.x;
+    int end = start + blockDim.x;
 
-inline void gpuAssert(cudaError_t code, const char* file, int line, bool abort = true)
-{
-    if (code != cudaSuccess) {}
-    {
-        fprintf(stderr, "GPUassert: %s %s %d\n\r", cudaGetErrorString(code), file, line);
-        if (abort) exit(code);
-    }
-}
-__host__ void check_CUDA_error(const char* e) {
-    cudaError_t error;
-    cudaDeviceSynchronize();
-    error = cudaGetLastError();
-    if (error != cudaSuccess)
-    {
-        printf("\nERROR %d: %s (%s)", error, cudaGetErrorString(error), e);
-    }
-}
-
-#define TILE_DIM 16
-
-__global__ void transpos_no_SM(int* source, int* dest, int size)
-{
-
-    int i = threadIdx.x + blockIdx.x * blockDim.x;
-    int j = threadIdx.y + blockIdx.y * blockDim.y;
-
-    if (i < size && j < size)
-    {
-        int des_idx = i * size + j;
-        int src_idx = j * size + i;
-        dest[des_idx] = source[src_idx];
-    }
-}
-
-__global__ void transpose_Shared(int* source, int* dest, int size)
-{
-    __shared__ int tile[TILE_DIM][TILE_DIM + 1];
-
-    // input threads idx
-    int i_in = threadIdx.x + blockIdx.x * blockDim.x;
-    int j_in = threadIdx.y + blockIdx.y * blockDim.y;
-    
-    //input index
-    int src_idx = j_in * size + i_in;
-
-    // 1D index calculation shared memory
-    int _1D_index = threadIdx.y * blockDim.x + threadIdx.x;
-
-    // col major row and col index calculation
-    int i_row = _1D_index / blockDim.y;
-    int i_col = _1D_index % blockDim.y;
-
-    //coordinate for transpose matrix
-    int i_out = blockIdx.y * blockDim.y + threadIdx.x;
-    int j_out = blockIdx.x * blockDim.y + threadIdx.y;
-
-    //output index
-    int dst_idx = j_out * size + i_out;
-
-    if (i_in < size && j_in < size)
-    {
-        //load from in array in row major and store to shared
-        tile[threadIdx.y][threadIdx.x] = source[src_idx];
-
-        // wait untill all the threads load the values 
-        __syncthreads();
-
-        dest[dst_idx] = tile[threadIdx.x][threadIdx.y];
+    if (end > n) {
+        end = n;
     }
 
+    for (int i = threadIdx.x; i < BLOCK_SIZE; i += blockDim.x) {
+        if (i + start < n) {
+            shared_a[i] = a[i + start];
+        }
+    }
 
-}
+    __syncthreads();
 
-
-__global__ void convolution(int* a, int* k, int* c, int n, int kSize) {
-    int row = threadIdx.y;
-    int col = threadIdx.x;
-
-    int suma = 0;
-    if (row > 0 && row < n - 1 && col>0 && col < n - 1) {
-        for (int i = 0; i < kSize; i++) {
-            for (int j = 0; j < kSize; j++) {
-                suma += (a[(row)*n + i + (col)+j] * k[i * kSize + j]);
+    for (int i = start; i < end; i++) {
+        if (shared_a[i - start] == find) {
+            int old_pos = atomicExch(pos, i);
+            if (old_pos == -1) {
+                return;
             }
         }
-        c[row * n + col] = suma;
     }
 }
 
 int main() {
+    int size = 32;
+    int find = 24;
+    int* h_a, * res, * pos;
+    int* d_a, * d_pos;
 
-    const int n = 6;
-    int size = n * n * sizeof(int);
-    int* host_a, * host_c;
-    int* dev_a, * dev_c ;
-    
-    host_a = (int*)malloc(size);
-    host_c = (int*)malloc(size);
-    
-    cudaMalloc(&dev_a, size);
-    check_CUDA_error("Error");
-    cudaMalloc(&dev_c, size);
-    check_CUDA_error("Error");
+    h_a = (int*)malloc(size * sizeof(int));
+    pos = (int*)malloc(sizeof(int));
+    pos[0] = -1;
+    res = (int*)malloc(size * sizeof(int));
 
-    for (int i = 0; i < n * n; i++) {
-        int r = (rand() % (3));
-        host_a[i] = i;
-        host_c[i] = 0;
+    cudaMalloc(&d_a, size * sizeof(int));
+    cudaMalloc(&d_pos, sizeof(int));
+
+    srand(time(NULL));
+
+        for (int i = 0; i < size; i++) {
+        int r1 = (size - i);
+        h_a[i] = r1;
+        printf("%d ", h_a[i]);
     }
 
-    cout << "old:\n";
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            printf("%d ", host_a[i * n + j]);
-        }
-        printf("\n");
-    }
+    printf("\n");
+
+    cudaMemcpy(d_a, h_a, size * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_pos, pos, sizeof(int), cudaMemcpyHostToDevice);
+
+    dim3 grid(size >= 1024 ? size / 1024 : 1);
+    dim3 block(1024);
+
+    printf("Valor a encontrar: ");
 
 
-    cudaMemcpy(dev_a, host_a, size, cudaMemcpyHostToDevice);
-    check_CUDA_error("Error");
-    cudaMemcpy(dev_c, host_c, size, cudaMemcpyHostToDevice);
-    check_CUDA_error("Error");
-
-    dim3 grid(2, 2, 1);
-    dim3 block(n/2, n/2, 1);
-    //convolution << <grid, block >> > (dev_a, dev_kernel, dev_c, n, kLength);
-    transpose_Shared << <grid, block >> > (dev_a,dev_c,(n));
-    check_CUDA_error("Error");
-    cudaMemcpy(host_c, dev_c, size, cudaMemcpyDeviceToHost);
-    check_CUDA_error("Error");
+    search << <grid, block >> > (d_a, size, d_pos, find);
 
     cudaDeviceSynchronize();
-    check_CUDA_error("Error");
-    cudaDeviceReset();
-    check_CUDA_error("Error");
 
-    cout << "new:\n";
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << host_c[i * n + j] << " ";
-        }
-        cout << "\n";
+    cudaMemcpy(pos, d_pos, sizeof(int), cudaMemcpyDeviceToHost);
+
+    if (pos[0] == -1) {
+        printf("No se encontro el valor\n");
     }
-    free(host_a);
-    free(host_c);
+    else {
+        printf("Encontrado en el indice %d \n", pos[0]);
+    }
 
+    free(h_a);
+    free(pos);
+    free(res);
+    cudaFree(d_a);
+    cudaFree(d_pos);
 
-    //return 0;
+    return 0;
 }
